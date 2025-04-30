1d028b546cddb29761f8ddc3b72cfe4bafb75305
do007 <30993365+Icemans007@users.noreply.github.com>
2025-04-30 17:34:29 +0800

    Update _worker.js
    如果第一行是空行，设置为是追加内容

--- _worker.js
+++ _worker.js
@@ -81,6 +81,11 @@ async function handleFileOperation(KV, fileName, url, request, token) {
     else {
         content = text ?? base64Decode(replaceSpacesWithPlus(b64));
     }
+    // 如果第一行是空行，设置为是追加内容
+    if (!Boolean(content.split('\n')[0].trim())) {
+        const oldContent = await KV.get(fileName, { cacheTtl: 60 });
+        content = oldContent + content;
+    }
 
     await KV.put(fileName, content);
     const verifiedContent = await KV.get(fileName, { cacheTtl: 60 });
